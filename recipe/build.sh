99d7c00200f239cd58a2e82cdba5271aa497329c
Francesco Biscani <bluescarni@gmail.com>
2017-05-12 14:00:28 +0200

    Fix win build and disable tests on OSX for the moment.

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,14 +3,20 @@
 mkdir build
 cd build
 
+if [[ "$(uname)" == "Darwin" ]]; then
+    export ENABLE_TESTS=no
+else
+    export ENABLE_TESTS=yes
+fi
+
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=$PREFIX \
     -DCMAKE_PREFIX_PATH=$PREFIX \
     -DPAGMO_WITH_EIGEN3=yes \
-    -DPAGMO_WITH_NLOPT=yes  \
-    -DPAGMO_WITH_IPOPT=yes  \
-    -DPAGMO_BUILD_TESTS=yes \
+    -DPAGMO_WITH_NLOPT=yes \
+    -DPAGMO_WITH_IPOPT=yes \
+    -DPAGMO_BUILD_TESTS=$ENABLE_TESTS \
     -DPAGMO_BUILD_TUTORIALS=yes \
     ..
 
