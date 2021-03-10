b659e54beb400e61b0f649c1d5d8632ea45a35b7
Francesco Biscani <bluescarni@gmail.com>
2021-03-10 10:07:56 +0100

    Cleanup.

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,12 +4,10 @@ mkdir build
 cd build
 
 if [[ "$target_platform" == osx-* ]]; then
-    export ENABLE_TESTS=yes
     # Workaround for compile issue on older OSX SDKs.
     export CXXFLAGS="$CXXFLAGS -fno-aligned-allocation"
 else
     LDFLAGS="-lrt ${LDFLAGS}"
-    export ENABLE_TESTS=yes
 fi
 
 cmake ${CMAKE_ARGS} \
@@ -20,7 +18,7 @@ cmake ${CMAKE_ARGS} \
     -DPAGMO_WITH_EIGEN3=yes \
     -DPAGMO_WITH_NLOPT=yes \
     -DPAGMO_WITH_IPOPT=yes \
-    -DPAGMO_BUILD_TESTS=$ENABLE_TESTS \
+    -DPAGMO_BUILD_TESTS=yes \
     -DPAGMO_ENABLE_IPO=yes \
     -DPAGMO_BUILD_TUTORIALS=yes \
     ..
