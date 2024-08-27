7f361b1c2b8a1a5b49c352167d581e4b818e61f6
Julien Schueller <schueller@phimeca.com>
2024-08-27 11:02:02 +0200

    Use CMAKE_UNITY_BUILD

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,17 @@
-mkdir build
-cd build
-
 cmake ^
     -G "NMake Makefiles" ^
     -DCMAKE_BUILD_TYPE=Release ^
-    -DBoost_NO_BOOST_CMAKE=ON ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+    -DCMAKE_UNITY_BUILD=ON ^
     -DPAGMO_WITH_EIGEN3=yes ^
     -DPAGMO_WITH_NLOPT=yes ^
     -DPAGMO_WITH_IPOPT=yes ^
     -DPAGMO_BUILD_TESTS=yes ^
     -DPAGMO_BUILD_TUTORIALS=yes ^
     -DPAGMO_ENABLE_IPO=yes ^
-    ..
+    -B build .
 
-cmake --build . --config Release --target install
+cmake --build build --config Release --target install
 
-ctest
+ctest --test-dir build --output-on-failure
