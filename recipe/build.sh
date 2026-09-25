2eadd42cc4f5de8cc9da0dedce4c9b816d396298
Julien Schueller <schueller@phimeca.com>
2026-09-25 14:55:56 +0200

    Update to 2.20.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,17 +1,5 @@
 #!/usr/bin/env bash
 
-
-if [[ "$target_platform" == osx-* ]]; then
-    # Workaround for compile issue on older OSX SDKs.
-    export CXXFLAGS="$CXXFLAGS -fno-aligned-allocation"
-else
-    LDFLAGS="-lrt ${LDFLAGS}"
-fi
-
-if [[ "$target_platform" != "linux-ppc64le" && "$target_platform" != "osx-arm64" ]]; then
-  CMAKE_ARGS="${CMAKE_ARGS} -DPAGMO_ENABLE_IPO=ON"
-fi
-
 cmake ${CMAKE_ARGS} -G Ninja -LAH \
     -DCMAKE_BUILD_TYPE=Release \
     -DPAGMO_WITH_EIGEN3=yes \
